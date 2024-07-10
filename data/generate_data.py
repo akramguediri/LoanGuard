841296f9f3315699f4ce24c56b2fc5214ada2272
akramguediri <akramguediri2@gmail.com>
2024-07-10 19:36:16 +0200

    fixed the default equation and fixed the data

--- data/generate_data.py
+++ data/generate_data.py
@@ -7,11 +7,11 @@ def generate_data(num_records, file_path):
     data = []
     for _ in range(num_records):
         age = random.randint(18, 70)
-        income = random.randint(20000, 150000)
-        loan_amount = random.randint(5000, 50000)
+        income = random.randint(24000, 250000)
+        loan_amount = random.randint(5000, 500000)
         loan_term = random.choice([12, 24, 36, 48, 60])
-        credit_score = random.randint(300, 850)
-        default = random.choice([0, 1])
+        credit_score = random.randint(700, 850)
+        default = int((income * (loan_term / 12)) * 0.8 >= (loan_amount * 1.2) and credit_score >= 750)
         data.append([age, income, loan_amount, loan_term, credit_score, default])
     
     columns = ['age', 'income', 'loan_amount', 'loan_term', 'credit_score', 'default']
